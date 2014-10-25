807a7fbec10e8e514724d99104ffdd1b75406d65
Goran Savovski <gsavovski@gmail.com>
2014-10-25 08:53:22 -0500

    Gemfile, ruby-version, determine terminal size using ssty

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "http://rubygems.org"
+
+gem 'colorize'
+gem 'pry', '0.9.10'
+gem 'virb'
+gem 'thor'
+gem 'rspec'
--- game_of_life.rb
+++ game_of_life.rb
@@ -1,11 +1,12 @@
-class Board 
+class Board
 
-  
+  require 'pry'
+
   MODIFIERS = ([-1,0,1].product([-1,0,1]- [[0,0]])).freeze
   attr_accessor :board, :generation, :stats
-  @out = STDERR
 
   def initialize(board=[])
+    @out = $stdout
     @board = board
     puts MODIFIERS if ENV['debug']
     @board.map!{ |x,y| [x+10,y+20] }
@@ -16,7 +17,7 @@ class Board
   #                    [x,y]
   def find_neighbors_of(coordinate)
     #@stats[:t_find_neighbors_of_start] = Time.now
-    MODIFIERS.map do |x,y| 
+    MODIFIERS.map do |x,y|
       [coordinate[0] + x, coordinate[1] + y]
     end
     #@stats[:t_find_neighbors_of_end] = Time.now
@@ -34,7 +35,7 @@ class Board
   def print_board
     screen_x, screen_y = get_window_width_and_heiht
     output_string = ""
-    (screen_x-2).times do |i| 
+    (screen_x-2).times do |i|
  #output_string << ((i  == 0 ) ? j.to_s : "X")
       (screen_y-1).times do |j|
         if board.include? [i,j]
@@ -52,23 +53,16 @@ class Board
        end
     end
     @generation += 1
-    @out.print output_string 
+    @out.print output_string
     #@out.print "Generation: " + @generation.to_s + (@stats[:t_find_neighbors_of_start] - @stats[:t_find_neighbors_of_start]).to_s + "\n"
     @out.print "Generation: " + @generation.to_s + "\n"
     @out.flush
  #   sleep 0.001
   end
-   
-  #adapted from mr. Satoru Takabayashi's progress bar hosted on github
+
+  #Get terminal height and width (os x, ubuntu)
   def get_window_width_and_heiht
-    #detect terminal size on a mac
-    #`stty size`.scan(/\d+/).map { |s| s.to_i }.reverse
-    tiocgwinsz = 0x5413
-    data = [0, 0, 0, 0].pack("SSSS")
-    @out = STDERR
-    @out.ioctl(tiocgwinsz, data)
-    rows, cols, xpixels, ypixels = data.unpack("SSSS")
-    [rows,cols]
+    `stty size`.scan(/\d+/).map { |s| s.to_i }.reverse
   end
 
 end
@@ -80,23 +74,23 @@ class Game
 
     #setup = Struct.new(:name, :living_fields)
     #setup.new("acron", [[3,5],[5,6],[3,6],[4,8],[5,9],[5,10],[5,11]])
-    
+
     #@b = Board.new([[3,5],[5,6],[3,6],[4,8],[5,9],[5,10],[5,11]]) #acron
     @b = Board.new([[5,5],[5,6],[4,6],[6,6], [4,7]]) #F-pentomino
     #@b = Board.new([[5,5],[5,6],[4,5],[4,6], [4,10],[4,12],[6,11],[5,11]]) #die hard
     #@b = Board.new([[5,5],[5,6],[5,7],[5,8],[4,8],[3,8],[2,7],[4,4],[2,4]]) #lightweight spaceship
     #@b = Board.new([[3,3],[3,4],[3,5],[2,5],[1,4]]) #Glider
-    #@b = Board.new([[3,3],[3,4],[3,5]]) #blinker 
-  end 
+    #@b = Board.new([[3,3],[3,4],[3,5]]) #blinker
+  end
 
   def play(times=100)
   end
 
   def find_all_fields_to_visit
     @all_fields = []
-    @b.board.map do |x,y| 
+    @b.board.map do |x,y|
         puts @b.find_neighbors_of([x,y]).inspect if @debug
-        @b.find_neighbors_of([x,y]).each do |element| 
+        @b.find_neighbors_of([x,y]).each do |element|
           @all_fields << element
         end
     end
@@ -109,19 +103,19 @@ class Game
 
   def evolve_board
     @next_gen = []
-    find_all_fields_to_visit.each do |field|       
+    find_all_fields_to_visit.each do |field|
       #puts "*** field " + field.to_s
       if dead_fields.include? field  then
         @next_gen << field if ( @b.count_living_neighbors(field) == 3 )
         #@b.board << field if ( @b.count_living_neighbors(field) == 3 )
       else
         if [0,1].include? (@b.count_living_neighbors(field))
-           #puts "it dies" 
+           #puts "it dies"
         elsif [2,3].include? (@b.count_living_neighbors(field))
            #puts "zjivej"
            @next_gen << field
         else (@b.count_living_neighbors(field)) > 3
-           #puts "it dies" 
+           #puts "it dies"
         end
       end
     end
@@ -130,7 +124,7 @@ class Game
   end
 
 end
-  
+
 g = Game.new
 n = g.find_all_fields_to_visit
 puts n.inspect
@@ -141,4 +135,4 @@ g.b.print_board
 g.evolve_board
 #puts g.b.inspect
 }
- 
+
